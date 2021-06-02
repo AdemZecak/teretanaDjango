dfa601359be7dbb78272cc17419e2b28172da351
AdemZecak <adem_zecak@hotmail.com>
2021-06-02 23:04:57 +0200

    nove izmjene navbar

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name = "index"),
+    path('contact.html',views.contact, name = "contact"),
+    path('about-us.html',views.about, name = "about-us"),
+    path('blog.html',views.blog, name = "blog"),
+    path('services.html',views.services, name = "services"),
+    path('bmi-calculator.html',views.bmi, name = "bmi-calculator"),
 ]
--- website/views.py
+++ website/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 
 
+
 def index(request):
     return render(request, 'index.html',{})
 
 def register(request):
-    return render(request,'register.html')
+    return render(request,'register.html',{})
+
+def contact(request):
+    return render(request, 'contact.html',{})
+
+def about(request):
+    return render(request, 'about-us.html',{})
+
+def blog(request):
+    return render(request, 'blog.html',{})
+
+def services(request):
+    return render(request, 'services.html',{})
+
+def bmi(request):
+    return render(request, 'bmi-calculator.html',{})
+
+
+
